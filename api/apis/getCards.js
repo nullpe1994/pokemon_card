bf941cb1405a76ec863eddde59c515e94f59e95e
torabit <pujigami@gmail.com>
2021-06-16 15:16:42 +0900

    Create CardListApi

---
+++ api/apis/getCards.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express();
+
+exports.getCards = function(req, res, pool) {
+  pool.connect( (err, client) => {
+    if (err) {
+      console.log(err);
+    } else {
+        client.query(`SELECT pokemon_card_name, img_url FROM pokemon_card WHERE ISDELETE='0'`, (err, result) => {
+            console.log(result);
+            if (true) {
+              res.header('Access-Control-Allow-Origin', '*');
+              res.json([{
+                  url: '/decklist', // デッキリスト返還
+                  cardlist: result.rows,
+              }]);
+            } else {
+              res.json([{
+                  url: '/', // ログイン失敗後ページ返還
+                  errormsg: 'ログイン失敗しました'
+              }]);
+            }
+      });
+    }
+  });
+};
--- api/main.js
+++ api/main.js
@@ -3,6 +3,7 @@ const router = express();
 const port = 3001;
 const conection = require('./common/postgres.js');
 const login = require('./apis/login.js');
+const getCards = require('./apis/getCards.js');
 
 router.get('/login', (req, res) => {
     const pool = conection.conection();
@@ -12,6 +13,16 @@ router.get('/login', (req, res) => {
 router.post('/login', (req, res) => {
     const pool = conection.conection();
     login.login(req, res, pool);
+});
+
+router.get('/getcards', (req, res) => {
+    const pool = conection.conection();
+    getCards.getCards(req,res,pool);
+});
+
+router.post('/getcards', (req, res) => {
+    const pool = conection.conection();
+    getCards.getCards(req,res,pool);
 });
 
 router.listen(port, () => {
