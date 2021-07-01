3983dc8a6e9d3ca458bfe0f85edbefd9311a01e9
torabit <pujigami@gmail.com>
2021-07-02 04:08:54 +0900

    マリガン、サイドカードセットを追加しました。

--- client/src/components/Competitive.jsx
+++ client/src/components/Competitive.jsx
@@ -4,7 +4,16 @@ import { makeStyles, Box, Divider } from '@material-ui/core';
 import OpponentField from './OpponentField';
 import YourField from './YourField';
 import DrawACard from './function/DrawACard';
-import AcceptButton from './common/AcceptButton';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import ShuffleTheDeck from './function/ShuffleTheDeck';
+import {useRecoilState, useSetRecoilState} from 'recoil';
+import yourHandState from './State/yourHandState';
+import yourSideCardsState from './State/yourSideCardsState';
 
 const useStyles = makeStyles(() => ({
     root: {
@@ -23,20 +32,70 @@ const useStyles = makeStyles(() => ({
 const Competitive = (props) => {
     const classes = useStyles();  
     const deck = props.location.state.deck;
-    const [yourHand, setYourHand] = useState([]);
     const [acceptButtonName, setAcceptButtonName] = useState('先攻');
     const [declineButtonName, setDeclineButtonName] = useState('後攻');
+    const [yourHand, setYourHand] = useRecoilState(yourHandState);
+    const setYourSideCards = useSetRecoilState(yourSideCardsState);
     const [phase, setPhase] = useState('0');
+    const [noBasic, setNoBasic] = useState(false);
 
     const Draw = () => {
         const newCard = DrawACard(deck.cards);
-        setYourHand((prevArray)=>[...prevArray, newCard]);
+        setYourHand((prevArray) => [...prevArray, newCard]);
+        deck.cards.pop();
+        return newCard;
+    }
+    
+    console.log(deck.cards);
+    
+    const Mulligan = () => {
+        let card = "";
+        let basicCnt = 0;
+        
+        yourHand.map(card => {
+            deck.cards.push(card);
+        });
+        setYourHand([]);
+        for (let i=0; i<7; i++) ShuffleTheDeck(deck.cards);
+
+        for (let i=0; i<7; i++) {
+            card = Draw();
+            card.subtypes.filter((subtype) => {
+                if (subtype === 2) basicCnt++;
+            });
+        }
+
+        if (basicCnt === 0) setNoBasic(true);
+        else {
+            setNoBasic(false);
+            for (let i=0; i<6; i++) SetSideCards();
+        }
+
+    }
+    
+    const SetSideCards = () => {
+        const newCard = DrawACard(deck.cards);
+        setYourSideCards((prevArray) => [...prevArray, newCard]);
         deck.cards.pop();
     }
 
     const isCorrect = () => {
+        let card = "";
+        let basicCnt = 0;
+
         for (let i=0; i<7; i++) {
-            Draw();
+            card = Draw();
+            card.subtypes.filter((subtype) => {
+                if (subtype === 2) {
+                    basicCnt++;
+                }
+            });
+        }
+
+        if (basicCnt === 0) setNoBasic((prev) => !prev);
+        else {
+            setNoBasic(false);
+            for (let i=0; i<6; i++) SetSideCards();
         }
     }
 
@@ -56,6 +115,23 @@ const Competitive = (props) => {
                 yourHand={yourHand}
                 deck = {deck}
             />
+            <Dialog
+                open={noBasic}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">種ポケモンがいません</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        対戦相手に手札を公開し、マリガンをしてください。
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={Mulligan} color="secondary">
+                        はい
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </Box>
     );
 }
