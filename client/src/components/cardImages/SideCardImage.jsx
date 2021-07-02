ffdf103c863cb1c7a57b1ad8e63cfacbea096e8c
torabit <pujigami@gmail.com>
2021-07-02 20:14:40 +0900

    sidecardにTooltipを追加しました。

--- client/src/components/cardImages/SideCardImage.jsx
+++ client/src/components/cardImages/SideCardImage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import ButtonBase from '@material-ui/core/ButtonBase';
+import Tooltip from '@material-ui/core/Tooltip';
 import pokeUra from '../../image/poke_ura.jpg';
 
 const useStyles = makeStyles((theme) => ({
@@ -31,23 +32,25 @@ const useStyles = makeStyles((theme) => ({
 const SideCardImage = (props) => {
     const classes = useStyles();
     const sideCards = props.sideCards;
-    console.log(sideCards);
-
+    
     return (
-        <ButtonBase
-            focusRipple
-            className={classes.image}
-            style={{
-                width: 130,
-            }}
-            >
-            <span
-            className={classes.imageSrc}
-            style={{
-                backgroundImage: `url(${pokeUra})`,
-            }}
-            />
-        </ButtonBase>
+        // Tooltipは削除予定
+        <Tooltip title={"サイドカード: " + sideCards.card_name}>
+            <ButtonBase
+                focusRipple
+                className={classes.image}
+                style={{
+                    width: 130,
+                }}
+                >
+                <span
+                className={classes.imageSrc}
+                style={{
+                    backgroundImage: `url(${pokeUra})`,
+                }}
+                />
+            </ButtonBase>
+        </Tooltip>
     );
 }
 export default SideCardImage;
